2d1e71bb5766ae2f6aaf17b76ec125e602260fe7
marcoosdantas <2226973@aluno.univesp.br>
2023-09-29 16:30:30 -0300

    first commit

---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,7 @@
+package com.generation.blogpessoal.repository;
+
+import com.generation.blogpessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+}
